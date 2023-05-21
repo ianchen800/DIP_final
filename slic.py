902632c7285479667e75fcf3c68c523f10cc246b
ianchen800 <56215478+ianchen800@users.noreply.github.com>
2023-05-22 05:33:46 +0800

    Add files via upload

--- slic.py
+++ slic.py
@@ -13,7 +13,7 @@ def Show(name, img):
     cv2.destroyWindow(name)
 
 class Cluster(object):
-    cluster_index = 1
+    cluster_index = 0
     def __init__(self, l, a, b, i, j):
         self.update(l, a, b, i, j)
         self.pixels = [] # 存哪些pixels是屬於該cluster的
@@ -27,42 +27,43 @@ class SLIC(object):
     def __init__(self, path, k, m, max_iter=10):
         # 0 ≤ l ≤ 100 , −127 ≤ a ≤ 127, −127 ≤ b ≤ 127 
         self.img = cv2.imread(path)
-        self.data = cv2.cvtColor(self.img, cv2.COLOR_BGR2LAB)
-        # self.data = torch.from_numpy(cv2.cvtColor(self.img, cv2.COLOR_BGR2LAB))
-        # print(self.data)
+        self.lab = cv2.cvtColor(self.img, cv2.COLOR_BGR2LAB)
+        # self.lab = torch.from_numpy(cv2.cvtColor(self.img, cv2.COLOR_BGR2LAB))
+        # print(self.lab)
         self.k = k
         self.m = m
         self.max_iter = max_iter
-        self.N = self.data.shape[0] * self.data.shape[1]
+        self.h = self.lab.shape[0]
+        self.w = self.lab.shape[1]
+        self.N = self.h * self.w
         self.S = int((self.N/self.k)**0.5)
         # print(f'S = {self.S}')
 
         self.clusters = []
-        self.label = {} # dictionary
-        self.label_arr = np.zeros((self.data.shape[0], self.data.shape[1]))
 
+        # Set label l(i) = -1 for each pixel i.
+        self.label = np.full((self.h, self.w), -1)
         # Set distance d(i) = inf for each pixel i.
-        self.dis = np.full((self.data.shape[0], self.data.shape[1]), np.inf)
+        self.dis = np.full((self.h, self.w), np.inf)
 
     # Initialize cluster centers C_k = [l_k,a_k,b_k,x_k,y_k]
     # by sampling pixels at regular grid steps S.
     def init_clusters(self):
-        for i in range(self.S//2, self.data.shape[0], self.S):
-            for j in range(self.S//2, self.data.shape[1], self.S):
-                self.clusters.append(Cluster(self.data[i,j][0],
-                                             self.data[i,j][1],
-                                             self.data[i,j][2], i, j))
-
+        for i in range(self.S//2, self.h, self.S):
+            for j in range(self.S//2, self.w, self.S):
+                self.clusters.append(Cluster(self.lab[i,j][0],
+                                             self.lab[i,j][1],
+                                             self.lab[i,j][2], i, j))
     # why?
     def get_gradient(self, i, j):
-        if i+1 >= self.data.shape[0]:
-            i = self.data.shape[0]-2
-        if j+1 >= self.data.shape[1]:
-            j = self.data.shape[1]-2
+        if i+1 >= self.h:
+            i = self.h-2
+        if j+1 >= self.w:
+            j = self.w
 
-        gradient = self.data[i+1,j+1][0]-self.data[i,j][0] + \
-                   self.data[i+1,j+1][1]-self.data[i,j][1] + \
-                   self.data[i+1,j+1][2]-self.data[i,j][2]
+        gradient = self.lab[i+1,j+1][0]-self.lab[i,j][0] + \
+                   self.lab[i+1,j+1][1]-self.lab[i,j][1] + \
+                   self.lab[i+1,j+1][2]-self.lab[i,j][2]
         # print(gradient)
         return gradient
 
@@ -76,18 +77,18 @@ class SLIC(object):
                     _j = cluster.j + dj
                     new_gradient = self.get_gradient(_i, _j)
                     if new_gradient < cluster_gradient:
-                        cluster.update(self.data[_i][_j][0], self.data[_i][_j][1], self.data[_i][_j][2], _i, _j)
+                        cluster.update(self.lab[_i][_j][0], self.lab[_i][_j][1], self.lab[_i][_j][2], _i, _j)
                         cluster_gradient = new_gradient
 
-    def assignment(self): # 最花時間 
+    def assignment(self): # 最花時間
         for cluster in self.clusters:
             for i in range(cluster.i-self.S, cluster.i+self.S):
-                if i < 0 or i >= self.data.shape[0]:
+                if i < 0 or i >= self.h:
                     continue
                 for j in range(cluster.j-self.S, cluster.j+self.S):
-                    if j < 0 or j >= self.data.shape[1]: 
+                    if j < 0 or j >= self.w: 
                         continue
-                    l, a, b = self.data[i,j]
+                    l, a, b = self.lab[i,j]
 
                     dc = ((l-cluster.l)**2 + (a-cluster.a)**2 + (b-cluster.b)**2)**0.5
                     ds = ((i-cluster.i)**2 + (j-cluster.j)**2)**0.5
@@ -96,12 +97,11 @@ class SLIC(object):
                         self.dis[i,j] = D
 
                         # set l(i) = k
-                        if (i, j) in self.label:
-                            self.label[(i, j)].pixels.remove((i, j))
-                        self.label[(i, j)] = cluster
+                        # 如果這個label已經存在其他cluster裡面了，就要把舊的刪掉
+                        if self.label[i,j] != -1:
+                            self.clusters[self.label[i,j]].pixels.remove((i,j))
+                        self.label[i,j] = cluster.no
                         cluster.pixels.append((i, j))
-
-                        self.label_arr[i,j] = cluster.no
 
     def update_cluster(self):
         for cluster in self.clusters:
@@ -109,9 +109,9 @@ class SLIC(object):
             number = len(cluster.pixels)
             # print(f'number = {number}')
             for p in cluster.pixels:
-                sum_l += self.data[p[0]][p[1]][0]
-                sum_a += self.data[p[0]][p[1]][1]
-                sum_b += self.data[p[0]][p[1]][2]
+                sum_l += self.lab[p[0]][p[1]][0]
+                sum_a += self.lab[p[0]][p[1]][1]
+                sum_b += self.lab[p[0]][p[1]][2]
                 sum_i += p[0]
                 sum_j += p[1]
             _l, _a, _b = int(sum_l/number), int(sum_a/number), int(sum_b/number)
@@ -123,7 +123,7 @@ class SLIC(object):
             self.assignment()
             self.update_cluster()
 
-        image_arr = np.copy(self.data)
+        image_arr = np.copy(self.lab)
         for cluster in self.clusters:
             for p in cluster.pixels:
                 image_arr[p[0]][p[1]][0] = cluster.l
@@ -142,12 +142,12 @@ class SLIC(object):
         contour = np.copy(self.img)
         di = [0, 1]
         dj = [1, 0]
-        for i in range(self.data.shape[0]):
-            for j in range(self.data.shape[1]):
+        for i in range(self.h):
+            for j in range(self.w):
                 for k in range(len(di)):
-                    if not (0 <= i+di[k] < self.data.shape[0] and 0 <= j+dj[k] < self.data.shape[1]):
+                    if not (0 <= i+di[k] < self.h and 0 <= j+dj[k] < self.w):
                         continue
-                    if self.label_arr[i,j] != self.label_arr[i+di[k], j+dj[k]]:
+                    if self.label[i,j] != self.label[i+di[k], j+dj[k]]:
                         contour[i,j] = [0,0,0]
         name = f'lenna_k{self.k}_m{self.m}_loop{self.max_iter}_contour.png'
         Show(name, contour)
@@ -168,13 +168,13 @@ if __name__ == '__main__':
     # 10 iterations suffices for most images
     # When using the CIELAB color space, m can be [1, 40].
     # k=4096, 512, 
-    p = SLIC(path='./Lenna.png', k=512, m=40, max_iter=5)
+    p = SLIC(path='./Lenna.png', k=1024, m=40, max_iter=5)
     p.init_clusters()
     p.move_clusters()
     # print(f'finish move_clusters')
     p.repeat()
-    label_arr = p.label_arr
-    # print(label_arr)
+    label = p.label
+    # print(label)
     contour = p.draw_contour()
 
     # opencv 的 SLIC
